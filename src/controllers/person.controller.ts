0103a10cb1db1632e2afdb91cc99bbbe4377f064
SanjeevSinjali <tdkzbiru@gmail.com>
2026-04-24 09:31:18 +0545

    Did some changes

--- src/controllers/person.controller.ts
+++ src/controllers/person.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { data } from "../models/person.model";
+import { HttpException } from "../exception/http-exception";
+
 
 export class PersonController {
     getALlPerson(arg0: string, getALlPerson: any) {
@@ -9,13 +11,14 @@ export class PersonController {
     async getAllPerson(req: Request, res: Response) {
         try {
             const someVar: any = {};
-
+            //implement exception handling
+            if (!someVar.name) {
+                throw new HttpException(400, "Name is required");
+            }
             // simulate error
             someVar.name.getAll();
-
             return res.status(200).json(data);
-
-        } catch (err: any) {
+        } catch (err: Error | unknown) {
             return res.status(500).json({ message: "Failed to get" });
         }
     }
